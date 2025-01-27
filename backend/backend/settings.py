296d048dd6a805f984dda7e66ec382ec575e9016
Ivan <ivan.artemov.04@gmail.com>
2025-01-27 19:46:09 +0300

    2nd commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '84.201.154.1', 'prjct.hopto.org']
+ALLOWED_HOSTS = ['localhost', '84.201.154.1',
+                 'taskiprod.hopto.org', '127.0.0.1']
 
 
 # Application definition
